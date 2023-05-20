d98969ce526e5bc40fde0f930061f64b14a6a032
Mariana Mena <maranashokry@ahoo.com>
2023-05-20 12:19:45 -0500

    Update GetUserById( )

--- RXCareServer/RXCareServer/Repositories/CommentRepository.cs
+++ RXCareServer/RXCareServer/Repositories/CommentRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace RXCareServer.Repositories
+﻿using ReadersRendezvous.Utils;
+using RXCareServer.Models;
+
+namespace RXCareServer.Repositories
 {
-    public class CommentRepository
+    public class CommentRepository : BaseRepository, ICommentRepository
     {
+        public CommentRepository(IConfiguration configuration) : base(configuration) { }
+
+
+        //------------------------------Backend-GetPatientComment( )#33-----------------------------------
+
+        public CommentInfo GetPatientComment(int id)
+        {
+            using(var conn = Connection)
+            {
+                conn.Open();
+                using(var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"";
+                    DbUtils.AddParameter(cmd, "@Id", id);
+                    var reader = cmd.ExecuteReader();
+
+
+
+
+
+
+                }
+            }
+
+            return new CommentInfo { };
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
