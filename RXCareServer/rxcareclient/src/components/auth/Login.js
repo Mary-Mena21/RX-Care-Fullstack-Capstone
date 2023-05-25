9b83dcdf676b77e88d2b86cfe2abea986d01ac76
Mariana Mena <maranashokry@ahoo.com>
2023-05-25 11:36:19 -0500

    Register finished

--- RXCareServer/rxcareclient/src/components/auth/Login.js
+++ RXCareServer/rxcareclient/src/components/auth/Login.js
@@ -2,68 +2,68 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css"
+import "./Login.css";
 import LoginBackGroundPhoto from "../images/LoginBackGroundPhoto.jpg";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+    const [Email, setEmail] = useState("");
+    const [Type, setType] = useState("");
 
-  const [Email, setEmail] = useState("");
-  const [Type, setType] = useState("");
+    const navigate = useNavigate();
 
+    const validateLogin = async () => {
+        try {
+            const response = await fetch(
+                `https://localhost:7183/api/Login/loginvalidate`,
+                {
+                    body: JSON.stringify({
+                        email: Email,
+                        type: Type,
+                    }),
+                    credentials: "include",
+                    method: "post",
+                    headers: { "Content-Type": "application/json" },
+                }
+            );
 
-  const navigate = useNavigate();
+            const loginResponse = await response.json();
+            console.log(loginResponse);
+            //if (loginResponse.user) {
+            if (response.ok) {
+                const userData = { ...loginResponse.user };
+                console.log(userData);
 
-  const validateLogin = async () => {
-    try {
-      const response = await fetch(
-        `https://localhost:7183/api/Login/loginvalidate`,
-        {
-          body: JSON.stringify({
-            email: Email,
-            type: Type,
-          }),
-          credentials: "include",
-          method: "post",
-          headers: { "Content-Type": "application/json" },
+                userData.isAdmin = userData.userType === "Admin" ? true : false;
+                localStorage.setItem("app_user", JSON.stringify(userData));
+                navigate("/");
+            } else {
+                console.log(response);
+                window.alert("Invalid login");
+                throw new Error(`Error! status: ${response.status}`);
+            }
+        } catch (error) {
+            console.log(error);
         }
-      );
+    };
+    const submissionHandler = (event) => {
+        event.preventDefault();
+        validateLogin();
+    };
 
-      const loginResponse = await response.json();
-      console.log(loginResponse);
-      //if (loginResponse.user) {
-        if (response.ok) {
-    const userData = { ...loginResponse.user};
-          console.log(userData);
-
-         userData.isAdmin = userData.userType === "Admin" ? true : false;
-        localStorage.setItem("app_user", JSON.stringify(userData));
-          navigate("/");
-      } else {
-        console.log(response);
-        window.alert("Invalid login");
-        throw new Error(`Error! status: ${response.status}`);
-     }
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const submissionHandler = (event) => {
-    event.preventDefault();
-    validateLogin();
-  };
-
-  return (
-    <>
-    <div className="Container">
-      <div className="ImageContainer">
-        <div className="shadow"></div>
-        <h1 className="LoginHeader">RX-Care</h1>
-        <img className="LoginImage" src={LoginBackGroundPhoto} />
-        <div className="LoginQuote">"Welcome To RX-CARE"<br/> --</div>
-      </div>
-      <div className="InputContainer">
+    return (
+        <>
+            <div className="Container">
+                <div className="ImageContainer">
+                    <div className="shadow"></div>
+                    <h1 className="LoginHeader">RX-Care</h1>
+                    <img className="LoginImage" src={LoginBackGroundPhoto} />
+                    <div className="LoginQuote">
+                        "Welcome To RX-CARE"
+                        <br /> --
+                    </div>
+                </div>
+                {/*       <div className="InputContainer">
         <div className="emailInput">
           <h3>Email</h3>
           <input
@@ -77,12 +77,57 @@ export const Login = () => {
         <div className="button" onClick={(e) => submissionHandler(e)}>
         Login
         </div>
-        {/* <section className="link--register"> */}
+         <section className="link--register"> 
         <Link to="/register">NEW Patient?</Link>
-   {/*  </section> */}
+    </section> 
         </div>
-        </div>
-    </div>
-    </>
-  );
+        </div> */}
+
+                {/* <div class="container"> */}
+                <div class="InputContainer">
+                    <div class="login-form">
+                        <div class="main-div">
+                            <div class="panel">
+                                <h2>Login for RX-Care</h2>
+                                <p>Please enter your email</p>
+                            </div>
+                            <form id="Login">
+                                <div class="form-group">
+                                    <input
+                                        type="email"
+                                        class="form-control"
+                                        id="inputEmail"
+                      placeholder="Email Address"
+                      value={Email}
+                      onChange={(event) => setEmail(event.target.value)}
+                                    />
+                                </div>
+
+                                {/*                                 <div class="form-group">
+                                    <input
+                                        type="password"
+                                        class="form-control"
+                                        id="inputPassword"
+                                        placeholder="Password"
+                                    />
+                                </div> */}
+                                <div class="forgot">
+                                    <Link to="/register">
+                                        Register NEW Patient?
+                                    </Link>
+                                </div>
+                                <button
+                                    type="submit"
+                                    class="btn btn-primary"
+                                    onClick={(e) => submissionHandler(e)}
+                                >
+                                    Login
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
